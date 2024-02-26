9a5d593d90918ac0502610d8c810ef20ed1dc3d2
nobu1082 <nk.kusaba@gmail.com>
2024-02-26 23:45:22 +0900

    Delete src directory

--- src/main/java/com/example/samuraitravel/repository/OkiniiriRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.samuraitravel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.samuraitravel.entity.Okiniiri;
-
-public interface OkiniiriRepository extends JpaRepository<Okiniiri, Integer> {
-	public Okiniiri findByName(String name); 
-}
--- src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
- const stripe = Stripe('pk_test_51OLEXaJsSQOmZTMCCtIZstRDsg1FuX1FZkhHP66OszEnfqiJYkraKvjf6btmDUH7qsg9Rnah2euJMamhDMHGB7L4008PCwqVgC');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
